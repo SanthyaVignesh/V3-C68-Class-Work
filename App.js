4dec136705174df20169743a1d0f7eec01e938fd
SanthyaVignesh <santhyasvignesh@gmal.com>
2021-08-22 12:32:57 +0530

    C68

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import BottomNavigatorTab  from './components/BottomNavigatorTab';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return <BottomNavigatorTab/>
+  }
+  
 }
 
 const styles = StyleSheet.create({
